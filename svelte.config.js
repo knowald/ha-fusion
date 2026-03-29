a08ddf7f819a3a454a077315f3c503e0cf35451c
Kevin Nowald <kevin@nowald.dev>
2026-03-29 23:23:46 +0200

    chore: update dependencies for Svelte 5 migration

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { fastDimension } from 'svelte-fast-dimension';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), fastDimension()],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter()
 	},
